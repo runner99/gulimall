080846aa926ff22eba476679ce14b05d5eec4821
“runner99” <2825201723@qq.com>
2022-12-30 00:23:42 +0800

    P97

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/SkuInfoController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/SkuInfoController.java
@@ -35,7 +35,10 @@ public class SkuInfoController {
      */
     @RequestMapping("/list")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = skuInfoService.queryPage(params);
+//        PageUtils page = skuInfoService.queryPage(params);
+//
+//        return R.ok().put("page", page);
+        PageUtils page = skuInfoService.queryPageByCondition(params);
 
         return R.ok().put("page", page);
     }
--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,7 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -16,11 +18,32 @@ import com.atguigu.gulimall.ware.service.WareSkuService;
 @Service("wareSkuService")
 public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> implements WareSkuService {
 
+    @Autowired
+    WareSkuDao wareSkuDao;
+
+//    @Autowired
+//    ProductFeignService productFeignService;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        /**
+         * skuId: 1
+         * wareId: 2
+         */
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if(!StringUtils.isEmpty(skuId)){
+            queryWrapper.eq("sku_id",skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isEmpty(wareId)){
+            queryWrapper.eq("ware_id",wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
